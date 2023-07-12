e819109180625bfca9d80d4bb964aa229b95cbe4
Muhammad Najeeb Akram <Mnajeeb_Akram@hotmail.com>
2023-07-13 00:36:55 +0500

    Add Routes and Functions for Product, Category, and Subject

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 class ProductController extends Controller
 {
     public function add_new_product(Request $request){
+        
         $result = DB::table("product_tbl")->insert([
             "product_code" => $request->product_bar_code,
             "product_sname" => $request->product_short_name,
@@ -41,13 +42,100 @@ class ProductController extends Controller
     }
 
     public function get_products(Request $request){
+        
+        
         $products = DB::table("product_tbl")
             ->where("active", 1)
             ->orderBy("id", "DESC")
             ->get();
+            
         return response()->json([
             "success" => true,
             "products" => $products
         ]);
     }
+
+    public function get_category(Request $request){
+        $category = DB::table("category_tbl")
+        ->orderBy("id", "DESC")
+        ->get();
+
+        return response()->json([
+            "Success" => true,
+            "category" => $category
+        ]);
+    }
+
+    public function get_subjects(Request $request){
+        $subject = DB::table("subject_tbl")
+        ->orderBy("id", "DESC")
+        ->get();
+
+        return response()->json([
+            "Success" => true,
+            "subject" => $subject
+        ]);
+    }
+
+    public function get_sheet_sizes(Request $request){
+        $sheets = DB::table("sheet_size_tbl")
+        ->orderBy("id", "DESC")
+        ->get();
+
+        return response()->json([
+            "Success" => true,
+            "sheets" => $sheets
+        ]);
+    }
+
+    public function get_book_for_board(Request $request){
+        $boards = DB::table("book_for_board_tbl")
+        ->orderBy("id", "DESC")
+        ->get();
+
+        return response()->json([
+            "Success" => true,
+            "boards" => $boards
+        ]);
+
+    }
+
+    public function add_new_category(Request $request){
+        $result = DB::table("category_tbl")->insert([
+            "category" => $request -> category,
+        ]);
+        if ($result == 1 || $result == 0){
+            $categories = DB::table("category_tbl")
+                ->orderBy("id", "DESC")
+                ->get();
+            return response()->json([
+                "success" => 1,
+                "categories" => $categories
+            ]);
+        }else{
+            return response()->json([
+                "success" => 0
+            ]);
+        }
+    }
+
+    public function add_new_subject(Request $request){
+        $result = DB::table("subject_tbl")->insert([
+            "subject" => $request -> subject,
+        ]);
+        if ($result == 1 || $result == 0){
+            $subjects = DB::table("subject_tbl")
+                ->orderBy("id", "DESC")
+                ->get();
+            return response()->json([
+                "success" => 1,
+                "subjects" => $subjects
+            ]);
+        }else{
+            return response()->json([
+                "success" => 0
+            ]);
+        }
+    }
+    
 }
