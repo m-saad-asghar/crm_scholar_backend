66d3f428d96f6b278e6077aecd06e4b2e1336359
Muhammad Saad Asghar <saad@luxuryproperty.com>
2023-08-26 16:50:17 +0400

    users modules

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -58,4 +59,93 @@ class UserController extends Controller
             "user" => Auth::user()
         ]);
     }
+
+    public function get_users(Request $request){
+        $users = User::orderBy("id", "DESC")
+        ->where(function ($query) use ($request) {
+            if($request->search_term !== ""){
+                $query->where('users.name', 'LIKE', '%' . $request->search_term . '%')
+                    ->orWhere('users.email', 'LIKE', '%' . $request->search_term . '%')
+                    ->orWhere('users.phone', 'LIKE', '%' . $request->search_term . '%');
+            }
+        })
+        ->get();
+        return response()->json([
+            "users" => $users,
+            "success" => 1
+        ]);
+    }
+
+    public function change_status_user(Request $request, $id){
+        $result = DB::table("users")->where("id", $id)->update([
+            "active" => ($request->status == true) ? 1 : 0,
+        ]);
+        if ($result == 1){
+            return response()->json([
+                "success" => 1
+            ]);
+        }else{
+            return response()->json([
+                "success" => 0
+            ]);
+        }
+    }
+
+    public function add_new_user(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => 'repeat',
+                "error_messages" => $validator->errors()
+            ]);
+        }
+
+        $password = "123456";
+
+        $password_encrypted = bcrypt($password);
+        $user  = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $password_encrypted;
+        $user->active = 1;
+        $user->phone = $request->phone;
+        if($user->save()){
+            $users = User::orderBy("id", "DESC")->get();
+            return response()->json([
+                "success" => 1,
+                "users" => $users,
+                "message" => 'User is successfully created'
+            ]);
+        }else{
+            return response()->json([
+                "success" => 0
+            ]);
+        }
+
+}
+
+public function update_user(Request $request, $id){
+       $result = DB::table("users")->where("id", $id)->update([
+        "name" => $request->name,
+        "email" => $request->email,
+        "phone" => $request->phone
+    ]);
+    if ($result == 1 || $result == 0){
+        $users = DB::table("users")->orderBy('id', 'DESC')->get();
+        return response()->json([
+            "success" => 1,
+            "users" => $users
+        ]);
+    }else{
+        return response()->json([
+            "success" => 0
+        ]);
+    }
+}
+
 }
